a08d2301bf17d597b35a4796457904838a2432d9
Dave Amiana <amianism25@gmail.com>
2021-06-01 03:56:59 +0800

    constructors are edited in Comparable class

--- C++/comparable.h
+++ C++/comparable.h
@@ -3,6 +3,8 @@
 template <typename T> class Comparable{
     public:
         Comparable() = default;
+        Comparable(const T& other)  = delete;
+        Comparable(T&& other) = delete;
         virtual ~Comparable() = default;
 
         T& operator=(const T& other) = delete;
