3be5cc9b00e71ed0818c2cc4c89d51c485023096
Dave Amiana <amianism25@gmail.com>
2021-08-17 14:19:39 +0800

    modified unique pointer destructor

--- C++/AbstractContainers/Pointers.h
+++ C++/AbstractContainers/Pointers.h
@@ -10,58 +10,60 @@
  *
  */
 
-template <typename T> class unique_pointer {
+template <typename T> class unique_reference {
 
 public:
   T *m_ptr{nullptr};
-  explicit unique_pointer() = default;
-  explicit unique_pointer(const unique_pointer<T> &Type) = delete;
+  explicit unique_reference() = default;
+  explicit unique_reference(const unique_reference<T> &Type) = delete;
 
-  explicit unique_pointer(T *Type) : m_ptr(Type) {}
-  explicit unique_pointer(unique_pointer<T> &&other) noexcept {
+  explicit unique_reference(T *Type) : m_ptr(Type) {}
+  explicit unique_reference(unique_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  unique_pointer &operator=(const unique_pointer<T> &other) = delete;
-  unique_pointer &operator=(unique_pointer<T> &&other) noexcept {
+  unique_reference &operator=(const unique_reference<T> &other) = delete;
+  unique_reference &operator=(unique_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  ~unique_pointer() {
-    m_ptr->~T();
-    m_ptr = nullptr;
+  ~unique_reference() {
+    if (m_ptr != nullptr) {
+      m_ptr->~T();
+      m_ptr = nullptr;
+    }
   }
 
   T &operator*(void) { return *(this->m_ptr); }
   T *operator->(void) { return this->m_ptr; }
-  T &operator&(unique_pointer<T> &other) { return other.m_ptr; }
+  T &operator&(unique_reference<T> &other) { return other.m_ptr; }
 
 private:
-  void swap(unique_pointer<T> &other) noexcept {
+  void swap(unique_reference<T> &other) noexcept {
     std::swap(m_ptr, other.m_ptr);
   }
 };
 
-template <typename T> class unique_pointer<T[]> {
+template <typename T> class unique_reference<T[]> {
   T *m_ptr{nullptr};
   size_t m_size{0};
 
 public:
-  explicit unique_pointer() = default;
-  explicit unique_pointer(const unique_pointer<T> &other) = delete;
+  explicit unique_reference() = default;
+  explicit unique_reference(const unique_reference<T> &other) = delete;
 
-  explicit unique_pointer(T *Type, size_t i_size)
+  explicit unique_reference(T *Type, size_t i_size)
       : m_ptr(Type), m_size(i_size) {}
-  explicit unique_pointer(unique_pointer<T> &&other) noexcept {
+  explicit unique_reference(unique_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  unique_pointer &operator=(const unique_pointer<T> &other) = delete;
-  unique_pointer &operator=(unique_pointer<T> &&other) noexcept {
+  unique_reference &operator=(const unique_reference<T> &other) = delete;
+  unique_reference &operator=(unique_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  ~unique_pointer() {
+  ~unique_reference() {
     if (m_ptr != nullptr) {
       for (size_t i = 0; i < m_size; ++i)
         m_ptr[i]->~T(); // calls the destructor of T.
@@ -71,7 +73,7 @@ public:
 
   T &operator*(void) { return *(this->m_ptr); }
   T *operator->(void) { return this->m_ptr; }
-  T &operator&(unique_pointer<T> &other) { return other.m_ptr; }
+  T &operator&(unique_reference<T> &other) { return other.m_ptr; }
 
   T &operator[](size_t idx) {
     if (idx > m_size)
@@ -86,36 +88,37 @@ public:
   }
 
 private:
-  void swap(unique_pointer<T> &other) noexcept {
+  void swap(unique_reference<T> &other) noexcept {
     std::swap(m_size, other.m_size);
     std::swap(m_ptr, other.m_ptr);
   }
 };
 
-template <typename T> class shared_pointer {
+template <typename T> class shared_reference {
   int m_counter{0};
   T *m_ptr{nullptr};
 
 public:
-  explicit shared_pointer() = default;
-  explicit shared_pointer(const shared_pointer<T> &other) = delete;
+  explicit shared_reference() = default;
+  explicit shared_reference(const shared_reference<T> &other) = delete;
 
-  explicit shared_pointer(T *i_ptr) : m_counter(m_counter + 1), m_ptr(i_ptr) {}
-  explicit shared_pointer(shared_pointer<T> &&other) noexcept {
+  explicit shared_reference(T *i_ptr)
+      : m_counter(m_counter + 1), m_ptr(i_ptr) {}
+  explicit shared_reference(shared_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  shared_pointer &operator=(shared_pointer<T> &&other) noexcept {
+  shared_reference &operator=(shared_reference<T> &&other) noexcept {
     other.swap(*this);
   }
-  shared_pointer &operator=(const shared_pointer<T> &other) = delete;
+  shared_reference &operator=(const shared_reference<T> &other) = delete;
 
-  shared_pointer &operator=(shared_pointer<T> other) noexcept {
+  shared_reference &operator=(shared_reference<T> other) noexcept {
     m_counter += 1;
     other.m_ptr = m_ptr;
   }
 
-  ~shared_pointer() {
+  ~shared_reference() {
     m_counter -= 1;
     if (m_counter == 0) {
       m_ptr->~T();
@@ -125,43 +128,43 @@ public:
 
   T &operator*(void) { return *(this->m_ptr); }
   T *operator->(void) { return this->m_ptr; }
-  T &operator&(shared_pointer<T> &other) { return other.m_ptr; }
+  T &operator&(shared_reference<T> &other) { return other.m_ptr; }
   T *get(void) { return this->m_ptr; }
 
   virtual int count() const { return m_counter; }
 
 private:
-  void swap(shared_pointer<T> &other) noexcept {
+  void swap(shared_reference<T> &other) noexcept {
     std::swap(m_ptr, other.m_ptr);
   }
 };
 
-template <typename T> class shared_pointer<T[]> {
+template <typename T> class shared_reference<T[]> {
   int m_counter{0};
   size_t m_size;
   T *m_ptr{nullptr};
 
 public:
-  explicit shared_pointer() = default;
-  explicit shared_pointer(T *i_ptr, size_t i_size)
+  explicit shared_reference() = default;
+  explicit shared_reference(T *i_ptr, size_t i_size)
       : m_counter(m_counter + 1), m_ptr(i_ptr), m_size(i_size) {}
 
-  explicit shared_pointer(const shared_pointer<T> &other) = delete;
-  explicit shared_pointer(shared_pointer<T> &&other) noexcept {
+  explicit shared_reference(const shared_reference<T> &other) = delete;
+  explicit shared_reference(shared_reference<T> &&other) noexcept {
     other.swap(*this);
   }
 
-  shared_pointer &operator=(shared_pointer<T> &&other) noexcept {
+  shared_reference &operator=(shared_reference<T> &&other) noexcept {
     other.swap(*this);
   }
-  shared_pointer &operator=(const shared_pointer<T> &other) = delete;
+  shared_reference &operator=(const shared_reference<T> &other) = delete;
 
-  shared_pointer &operator=(shared_pointer<T> other) noexcept {
+  shared_reference &operator=(shared_reference<T> other) noexcept {
     m_counter += 1;
     other.m_ptr = m_ptr;
   }
 
-  ~shared_pointer() {
+  ~shared_reference() {
     m_counter -= 1;
     if (m_counter == 0) {
       for (size_t i = 0; i < m_size; ++i)
@@ -172,35 +175,35 @@ public:
 
   T &operator*(void) { return *(this->m_ptr); }
   T *operator->(void) { return this->m_ptr; }
-  T &operator&(shared_pointer<T> &other) { return other.m_ptr; }
+  T &operator&(shared_reference<T> &other) { return other.m_ptr; }
   T &operator[](size_t idx) { return this->m_ptr[idx]; }
 
   T &get(void) { return *(this->m_ptr); }
 
 private:
-  void swap(shared_pointer<T[]> &other) noexcept {
+  void swap(shared_reference<T[]> &other) noexcept {
     std::swap(m_counter, other.m_counter);
     std::swap(m_ptr, other.m_ptr);
     std::swap(m_size, other.m_size);
   }
 };
 
-template <typename T> class weak_pointer : public shared_pointer<T> {
+template <typename T> class weak_reference : public shared_reference<T> {
   T *m_ptr{nullptr};
 
 public:
-  explicit weak_pointer(shared_pointer<T> &i_ptr) : m_ptr(i_ptr.get()) {}
+  explicit weak_reference(shared_reference<T> &i_ptr) : m_ptr(i_ptr.get()) {}
 
-  weak_pointer() = delete;
-  weak_pointer(const T &) = delete;
-  weak_pointer(T &&) = delete;
-  weak_pointer &operator=(T &&) = delete;
-  weak_pointer &operator=(const T &Type) = delete;
-  ~weak_pointer() { m_ptr = nullptr; };
+  weak_reference() = delete;
+  weak_reference(const T &) = delete;
+  weak_reference(T &&) = delete;
+  weak_reference &operator=(T &&) = delete;
+  weak_reference &operator=(const T &Type) = delete;
+  ~weak_reference() { m_ptr = nullptr; };
 
   T &operator*(void) { return *(this->m_ptr); }
   T *operator->(void) { return this->m_ptr; }
-  T &operator&(weak_pointer<T> &other) { return other.m_ptr; }
+  T &operator&(weak_reference<T> &other) { return other.m_ptr; }
 
   T *get(void) { return (this->m_ptr); }
 
